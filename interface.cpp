8f97ac56f6951cfd717382fd4224b5c6cd6fc078
S3VIC <62557969+S3VIC@users.noreply.github.com>
2020-04-28 22:18:41 +0200

    Interface upgrade 1
    Moving code from main function to interface class - upgrading functionality of that class

--- interface.cpp
+++ interface.cpp
@@ -15,11 +15,11 @@ void Interface::welcome_screen()
 void Interface::main_menu()
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),11);
-    cout << "*********************************************************************************************************" << endl;
-    cout << "\t\t\t\t\t\t MAIN MENU"
+    cout << "***********************************************************************************************************************" << endl;
+    cout << "\t\t\t\t\t\t\tMAIN MENU"
     "\n\n 1. Calculate particle mass. \n\n 2. Get element information. \n\n 3. About program. \n\n 4. Exit."
 
-    "\n\n *********************************************************************************************************";
+    "\n\n***********************************************************************************************************************";
 
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
     cout << "\n\n \t\t\t\t\t       Choose option: ";
@@ -41,8 +41,34 @@ void Interface::menu_2()
     cin >> a;
     Element p1(a);
     p1.info_general();
-    p1.info_type();
     p1.load_info();
+}
+void Interface::menu_3()
+{
+     //system("CLS");
+        cout << "\t\t\t\t\t  MASS CALCULATOR BY S3VIC" << endl << "Program inspired by the personal interest as a nanostructures"
+    " engineering student in Chemistry. The aim of this program is to make it easy to calculate total particle mass with significant precision and to get access  to  basic "
+    " information about each particle included in periodic table of chemical elements in form of a basic console application "
+    "which is planned to get upgraded to more advanced application in the near future."
+    "\n\t\t\t\t Information source: https://www.ptable.com/ ";
+}
+void Interface::exit_4()
+{
+    exit(0);
+}
+bool Interface::back_to_menu()
+{
+    char decision;
+    cout << "\n Want to go back to main menu? y/n ";
+    while(true)
+    {
+    cin >> decision;
+    if(decision == 'y') return true;
+    else if (decision == 'n') return false;
+    else continue;
+    }
+
+
 }
 void Interface::interface_clear()
 {
